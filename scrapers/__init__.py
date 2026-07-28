bd3666ad0c17324d6a4c9a2f42138178193b07ab
XINZHAZHA <lixin@owangxiaoming.com>
2026-07-28 17:19:23 +0800

    Add scrapers/__init__.py

---
+++ scrapers/__init__.py
@@ -0,0 +1,7 @@
+from scrapers.nmpa import scrape_nmpa
+from scrapers.weibo import scrape_weibo
+from scrapers.zhihu import scrape_zhihu
+from scrapers.rss_scraper import scrape_rss
+from scrapers.overseas import scrape_overseas
+
+__all__ = ["scrape_nmpa", "scrape_weibo", "scrape_zhihu", "scrape_rss", "scrape_overseas"]
